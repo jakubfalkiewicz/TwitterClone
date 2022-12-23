b4ce699f3add0176f4306bc38d86122836f4d5d9
Jakub Falkiewicz <kufal222@gmail.com>
2022-12-23 22:02:36 +0100

    lab12

---
+++ lab12/src/components/ContactInfo.vue
@@ -0,0 +1,47 @@
+<template>
+  <li>
+    <h2>{{ name }} <img v-if="this.friendProp" class="friendIcon" src="friends.png" alt=""></h2>
+    <button @click="showHideFriend">{{ addRemove }}</button>
+    <button @click="showHideDetails">Ukryj/Pokaż szczegóły</button>
+    <details-vue v-if="details" :phoneNumber="this.phoneNumber" :emailAddress="this.emailAddress">
+
+    </details-vue>
+  </li>
+</template>
+
+<script>
+import DetailsVue from "./Details.vue";
+
+export default {
+  props: ['name', 'phoneNumber', 'emailAddress', 'friend'],
+  data() {
+    return {
+      addRemove: this.friend ? "Usuń ze znajomych" : "Dodaj do znajomych",
+      friendProp: this.friend,
+      details: false
+    };
+  },
+  methods:{
+    showHideFriend(){
+      this.friendProp ? this.addRemove = "Dodaj do znajomych" : this.addRemove = "Usuń ze znajomych";
+      this.friendProp = !this.friendProp
+    },
+    showHideDetails(){
+      console.log("details")
+      console.log(this.details)
+      this.details = !this.details
+    }
+  },
+  components: {
+    DetailsVue
+  },
+};
+
+</script>
+
+<style>
+  .friendIcon {
+    margin-left: 10px;
+    width: 30px;
+  }
+</style>
---
+++ lab12/src/components/Details.vue
@@ -0,0 +1,20 @@
+<template>
+      <ul>
+        <li>
+          <strong>telefon:</strong>
+          {{ phoneNumber }}
+        </li>
+        <li>
+          <strong>e-mail:</strong>
+          {{ emailAddress }}
+        </li>
+      </ul>
+
+  </template>
+
+<script>
+export default {
+    props: ['phoneNumber', 'emailAddress'],
+
+};
+</script>
