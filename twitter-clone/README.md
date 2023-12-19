628b243796cac70fc91dc610fdc8afd6f29acee3
Jakub Falkiewicz <kufal222@gmail.com>
2023-12-19 02:07:29 +0100

    Add routing

--- twitter-clone/README.md
+++ twitter-clone/README.md
@@ -5,3 +5,20 @@ This template should help get you started developing with Vue 3 in Vite. The tem
 ## Recommended IDE Setup
 
 - [VS Code](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur) + [TypeScript Vue Plugin (Volar)](https://marketplace.visualstudio.com/items?itemName=Vue.vscode-typescript-vue-plugin).
+
+user: 
+-username
+-password
+-name
+-title
+-followed users
+-posts
+-comments
+
+post:
+-author
+-content
+-datetime
+-comments
+-reposts
+-views
--- twitter-clone/src/main.js
+++ twitter-clone/src/main.js
@@ -1,5 +1,22 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
+import "./style.css";
+import App from "./App.vue";
+import Home from "./views/Home.vue";
+import Login from "./views/Login.vue";
+import Register from "./views/Register.vue";
+import NotFound from "./views/NotFound.vue";
 
-createApp(App).mount('#app')
+const routes = [
+  { path: "/", component: Home },
+  { path: "/login", component: Login },
+  { path: "/register", component: Register },
+  { path: "/:pathMatch(.*)*", name: "NotFound", component: NotFound },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+createApp(App).use(router).mount("#app");
